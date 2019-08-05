2acb603fde614356df65bac6359dc6edc8647cf6
lumuscode <diego-freitas@impulso.network>
2019-08-05 06:33:51 -0300

    Alterando chat bot

--- bot.js
+++ bot.js
@@ -1,56 +1,23 @@
-const tmi = require('tmi.js')
-
-
-const options = {
-    options: {
-      debug: true
-    },
-
-    connection: {
-      reconnect: true
-    },
-
-    identify: {
-      username: "bot_sofia",
-      password: "oauth:ohoaqkxrw5t3152brd4pbcmxjfdxdm"
-    },
-
-  // Caso queira criar bots para outros canais.
+const TwitchBot = require('twitch-bot')
+ 
+const Bot = new TwitchBot({
+  username: 'bot_sofia',
+  oauth: 'oauth:ohoaqkxrw5t3152brd4pbcmxjfdxdm',
   channels: ['bot_sofia']
-}
-
-const client = new tmi.client(options)
-
-
-client.on('message', onMessageHandler)
-client.on('connected', onConnectedHandler)
-
-client.connect()
-
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignorar mensagens do bot
-
-  // Remover espaço em branco da mensagem de bate-papo
-  const commandName = msg.trim()
-
-  // Se o comando é conhecido, vamos executá-lo
-  if (commandName === '!teste') {
-    const command = rollTest();
-    client.say(target, `Voce executou um ${command}`)
-    console.log(`* Comando ${commandName} executado`)
-  } else {
-    console.log(`* Comando desconhecido ${commandName}`)
+})
+ 
+Bot.on('join', channel => {
+  console.log(`Canal associado: ${channel}`)
+})
+ 
+Bot.on('error', err => {
+  console.log(err)
+})
+ 
+Bot.on('message', chatter => {
+  if(chatter.message === '!teste') {
+    Bot.say('Comando executado com sucesso')
   }
-}
+})
 
-// Função chamada quando o comando "teste" é emitido
-function rollTest () {
-    return 'Comando !teste executado com sucesso'
-  }
-  
-  // Chamado toda vez que o bot se conecta ao chat do Twitch
-  function onConnectedHandler (addr, port) {
-    console.log(`* Conectado em ${addr}:${port}`)
-  }
 
-  
