a1cf85da5ee8ba5e4328c399507063a2b59fd950
lumuscode <diego-freitas@impulso.network>
2019-08-05 03:13:41 -0300

    Bot para twitch

---
+++ index.js
@@ -0,0 +1,54 @@
+const tmi = require('tmi.js')
+
+
+const options = {
+    options: {
+        debug: true
+    },
+
+    connection: {
+        reconnect: true
+    },
+
+    identify: {
+        username: "bot_sofia",
+        password: "4v9i53erm54mmtzts6th235x19vh7w"
+    },
+
+    // Caso queira criar bots para outros canais.
+    channels: ['LumusCode']
+}
+
+const client = new tmi.client(options)
+
+
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+
+client.connect()
+
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignorar mensagens do bot
+
+  // Remover espaço em branco da mensagem de bate-papo
+  const commandName = msg.trim()
+
+  // Se o comando é conhecido, vamos executá-lo
+  if (commandName === '!teste') {
+    const num = rollTest();
+    client.say(target, `Voce executou um ${num}`)
+    console.log(`* Comando ${commandName} executado`)
+  } else {
+    console.log(`* Comando desconhecido ${commandName}`)
+  }
+}
+
+// Função chamada quando o comando "teste" é emitido
+function rollTest () {
+    return 'Comando !teste executado com sucesso'
+  }
+  
+  // Chamado toda vez que o bot se conecta ao chat do Twitch
+  function onConnectedHandler (addr, port) {
+    console.log(`* Conectado em ${addr}:${port}`)
+  }
